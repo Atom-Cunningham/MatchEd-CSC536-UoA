45ce1b2ebdf116459d70ae8378524a7dfb5df55e
Krishna Prashanth <62010158+krishnaprashanth246@users.noreply.github.com>
2025-03-13 17:50:43 -0700

    dont create tables, schema if already exists; expand the role grants

--- db.sql
+++ db.sql
@@ -6,18 +6,18 @@ GRANT match_rw TO match_user;
 CREATE DATABASE match_db WITH OWNER = match_rw;
 
 \connect match_db;
-CREATE SCHEMA match_schema;
+CREATE SCHEMA  IF NOT EXISTS match_schema;
 
-CREATE TABLE match_schema.user (
+CREATE TABLE  IF NOT EXISTS match_schema.user (
     id SERIAL PRIMARY KEY,
     name VARCHAR,
     email VARCHAR
 );
 
 
-GRANT USAGE ON SCHEMA match_schema to match_rw;
-GRANT SELECT, INSERT, UPDATE, DELETE ON match_schema.user TO match_rw;
-GRANT USAGE ON ALL SEQUENCES IN SCHEMA match_schema TO match_rw;
+GRANT USAGE ON SCHEMA match_schema TO match_rw;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA match_schema TO match_rw;
+GRANT USAGE, SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA match_schema TO match_rw;
 
 
 
