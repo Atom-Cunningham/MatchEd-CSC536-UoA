e58e099a2a5cc01bfc01ba068b68e17a6df9da68
honestus-codice <laser@email.arizona.edu>
2025-03-21 15:50:40 -0700

    Database connected
    Backend is now connected to the database

--- backend/main.go
+++ backend/main.go
@@ -8,9 +8,15 @@ import (
 	"backend/routes"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Load environment variables from the .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
 	// init gorm
 	models.InitDB()
--- backend/models/db.go
+++ backend/models/db.go
@@ -2,8 +2,11 @@ package models
 
 import (
 	"fmt"
+	"log"
+	"os"
 
-	"github.com/jinzhu/gorm"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 // the name of the database.
@@ -12,21 +15,66 @@ var DB *gorm.DB
 
 // TODO initialize gorm with postgres using real dbname
 func InitDB() {
+
+	// get dsn
+	dsn := getDSN()
+
+	//TODO remove this print statement
+	//fmt.Println(dsn)
+
 	var err error
-	// Skip DB connection if it's not available yet
-	if isDatabaseAvailable() {
-		DB, err = gorm.Open("postgres", "user=username dbname=mydb sslmode=disable password=yourpassword")
-		if err != nil {
-			panic("failed to connect to database")
-		}
-		fmt.Println("Database connected")
-	} else {
-		fmt.Println("Database is not available, skipping initialization")
+	// try to connect to database with gorm
+	DB, err = gorm.Open(postgres.New(postgres.Config{DSN: dsn}), &gorm.Config{})
+	if DB == nil {
+		fmt.Println("DB is nil")
 	}
+	if err != nil {
+		fmt.Printf("Error connecting to database %v\n", err)
+		panic("failed to connect to database")
+	}
+
+	// double check the underlying database object
+	// and ping the database
+
+	if isDatabaseAvailable() {
+		fmt.Printf("Database connected: %s", DB.Name())
+	} else {
+		fmt.Println("Failed to ping database")
+	}
+
 }
 
-// TODO ping the database
+// TODO: there is some bug here with a nil pointer
+// isDatabaseAvailable checks if the database is available and returns true if connected
+// takes the dsn string
 func isDatabaseAvailable() bool {
-	// Check if the database is accessible, e.g., via pinging or a network check
-	return false // Simulate a condition where the DB is not yet available
+	if DB == nil {
+		log.Println("DB connection is not initialized")
+		return false
+	}
+
+	sqlDB, err := DB.DB() // Get the underlying *sql.DB object
+	if err != nil {
+		log.Println("Error getting underlying sql.DB object:", err)
+		return false
+	}
+
+	if err := sqlDB.Ping(); err != nil {
+		log.Println("Error pinging the database:", err)
+		return false
+	}
+
+	fmt.Println("Successfully connected to the database!")
+	return true
+}
+
+func getDSN() string {
+	// Get database connection details from environment variables
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"))
+	return dsn
 }
